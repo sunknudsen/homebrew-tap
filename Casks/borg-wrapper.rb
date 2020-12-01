d9ccbcc6fadbe35683b73be261ae54f1b0b447c3
Sun Knudsen <hello@sunknudsen.com>
2020-12-01 10:15:23 -0500

    Initial commit

---
+++ Casks/borg-wrapper.rb
@@ -0,0 +1,18 @@
+cask "borg-wrapper" do
+  version "0.0.1"
+  sha256 "0cd8baac2ddf2eba760931281321c5a8fcfaae5c3a4430520a5ba88fce3eb7dd"
+
+  url "https://github.com/sunknudsen/borg-wrapper/releases/download/v#{version}/borg-wrapper-#{version}.dmg"
+  appcast "https://github.com/sunknudsen/borg-wrapper/releases.atom"
+  name "Borg Wrapper"
+  desc "Borg Wrapper is a lightweight wrapper used the run (or schedule) Borg backups securely on macOS."
+  homepage "https://github.com/sunknudsen/borg-wrapper"
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
+  depends_on formula: "sunknudsen/tap/borgbackup-llfuse"
+
+  app "Borg Wrapper.app"
+
+  uninstall pkgutil: "com.sunknudsen.borg-wrapper"
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# Sun Knudsen’s [Homebrew](https://brew.sh/) tap
+
+## How do I install casks?
+
+```shell
+brew install --cask sunknudsen/tap/<cask>
+```
+
+Or `brew tap sunknudsen/tap` and then `brew install --cask <cask>`.
+
+## How do I install formulas?
+
+```shell
+brew install sunknudsen/tap/<formula>
+```
+
+Or `brew tap sunknudsen/tap` and then `brew install <formula>`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew’s documentation](https://docs.brew.sh).
