5b029701858db7873ca3f18149fd228874ceaae9
Sun Knudsen <hello@sunknudsen.com>
2025-10-21 16:49:02 -0400

    Updated wakeful formula to v1.1.0

--- Formula/wakeful.rb
+++ Formula/wakeful.rb
@@ -1,8 +1,8 @@
 class Wakeful < Formula
-  desc "Prevent automatic sleep and gracefully handle manual sleep by sending SIGINT to child processes."
+  desc "Prevent automatic sleep and gracefully handle manual sleep by sending SIGINT to child processes"
   homepage "https://github.com/sunknudsen/wakeful"
-  url "https://github.com/sunknudsen/wakeful/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0069208dc112af09a2dea8b4cf29a38dd0878d38c1ddfa8e4c3bcc9101079e66"
+  url "https://github.com/sunknudsen/wakeful/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "d1930695f58ce8376f547d2d253f7227dd687de18963ef85ab6edf1b719f0e65"
   license "MIT"
 
   depends_on macos: :sequoia
@@ -13,6 +13,6 @@ class Wakeful < Formula
   end
 
   test do
-    assert_match "Wakeful 1.0.0", shell_output("#{bin}/wakeful --version")
+    assert_match "Wakeful 1.1.0", shell_output("#{bin}/wakeful --version")
   end
 end
